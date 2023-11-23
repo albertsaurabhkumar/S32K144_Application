c29b6ba0d160cb03afe3c65b1b83a0a54f634711
SaurabhK <albertsaurabhkumar@gmail.com>
2023-11-23 15:53:17 +0530

    Added Flash driver files

--- app/src/main.c
+++ app/src/main.c
@@ -5,19 +5,21 @@
 **  
 **  Main program file for the GCC Compiler
 */
+
+/*   PortPCR(PORTA,PIN11);
+  PortPCR(PORTB,PIN4);
+  DataDirectionGPIO(PTB,PIN4);
+  DataDirectionGPIO(PTA,PIN11); */
+
 #include "main.h"
 #include "clock.h"
 #include "port.h"
+#include "wdog.h"
 #include "common.h"
 
 volatile uint32_t dlyCnt;
 volatile uint32_t counter;
 //volatile uint32_t a=10;
-void WDOG_disable (void) {
- WDOG->CNT=0xD928C520; /*Unlock watchdog*/
- WDOG->TOVAL=0x0000FFFF; /*Maximum timeout value*/
- WDOG->CS = 0x00002100; /*Disable watchdog*/
-}
 
 void delay(uint32_t a) {
   while(a>0){
@@ -28,26 +30,15 @@ void delay(uint32_t a) {
 
 int main(void) {
   initClk();
-  WDOG_disable();
-  
-  PortPCR(PORTB,PIN4);
-  PortPCR(PORTA,PIN6);
-  PortPCR(PORTA,PIN11);
+  DisableWDOG();
 
-  DataDirectionGPIO(PTB,PIN4);
+  PortPCR(PORTA,PIN6);
   DataDirectionGPIO(PTA,PIN6);
-  DataDirectionGPIO(PTA,PIN11);
 
   while(1)
   {
     ToggleGPIO(PTA,PIN6);
-    delay(720000);
-
-    ToggleGPIO(PTB,PIN4);
-    delay(720000);
-
-    ToggleGPIO(PTA,PIN11);
-    delay(720000);
+    delay(720);
     // Never returns from this loop 
   }
   return 0;
